67d89ff63e657a5052b7fba288985ab011b21a00
TheVu <thevudoan@gmail.com>
2017-10-25 19:57:48 +0700

    update

--- public/roomControllers.js
+++ public/roomControllers.js
@@ -9,33 +9,27 @@ var getRoomsOnPage = (page, key) =>{
   }).then((data) => {
     rooms = data.result;
     console.log(rooms);
+    let i;
     // $("#room_img").append(`<img alt="image" src="${rooms[currentRoom].images[0]}" class="media-object">`);
     // $("#room_name").append(rooms[currentRoom].name);
-    $("#main_info").append("<li class='span4 box-container'>" +
-      "<div class='holder'>" +
-            "<a class='overlay' title='property title' href='#'>" +
-              "<span class='more'></span>" +
-              `<img alt='image' src='${rooms[currentRoom].images[1]}' class='media-object'>` +
-            "</a>" +
-            "<div class='prop-info'>" +
-                `<h3 class='prop-title'>${rooms[currentRoom].name}</h3>` +
-                "<ul class='more-info clearfix'>" +
-                    `<li class='info-label clearfix'><span class='pull-left'>Price:</span> <span class='qty pull-right'>${rooms[currentRoom].price}</span></li>` +
-                "</ul>" +
-            "</div>" +
-        "</div>" +
-    "</li>");
-    currentRoom++;
+    for (i=0;i<rooms.length;i++) {
+      $("#main_info").append("<li class='span4 box-container'>" +
+        "<div class='holder'>" +
+              "<a class='overlay' title='property title' href='#'>" +
+                "<span class='more'></span>" +
+                `<img alt='image' src='${rooms[i].images[1]}' class='media-object'>` +
+              "</a>" +
+              "<div class='prop-info'>" +
+                  `<h3 class='prop-title'>${rooms[i].name}</h3>` +
+                  "<ul class='more-info clearfix'>" +
+                      `<li class='info-label clearfix'><span class='pull-left'>Price:</span> <span class='qty pull-right'>${rooms[currentRoom].price}</span></li>` +
+                  "</ul>" +
+              "</div>" +
+          "</div>" +
+      "</li>");
+    }
   }).fail((err) => {
     console.error(err);
   });
 }
 getRoomsOnPage(1);
-getRoomsOnPage(1);
-getRoomsOnPage(1);
-getRoomsOnPage(1);
-
-// rooms.forEach(function(value) {
-//   getRoomsOnPage(1);
-//   console.log('Hi');
-// })
