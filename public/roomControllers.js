f97d71adbf49a99c76289d5aa0c98119d888c29c
TheVu <thevudoan@gmail.com>
2017-11-05 00:51:50 +0700

    update

--- public/roomControllers.js
+++ public/roomControllers.js
@@ -1,5 +1,12 @@
 var rooms = [];
 var numberOfRooms;
+var currentPage = 2;
+var currentSortPage = 1;
+var isLoading = false;
+var loadDone = false;
+var isSort = false;
+var isAscend = false;
+var isDescend = false;
 var query = window.location.search.substring(1);
 var re = query==='' ? '' : /w/gi;
 var newQuery = query.replace(re, ' ');
@@ -7,32 +14,79 @@ var newQuery = query.replace(re, ' ');
 var source = document.getElementById('entry-template').innerHTML;
 var template = Handlebars.compile(source);
 var getRoomsOnPage = (page, key) => {
+  isSort = false;
+  isLoading = true;
   $.ajax({
     type  : 'get',
     url   : 'https://murmuring-anchorage-78399.herokuapp.com/api/rooms' + "?page=" + page + "&key=" + key
   }).then((data) => {
     rooms = data.result;
+    if (rooms.length===0) loadDone = true;
     console.log(rooms);
     var test = template({obj: rooms});
-    document.getElementById('main_info').innerHTML = test;
+    $('#main_info').append(test);
   }).fail((err) => {
     console.error(err);
+  }).always(() => {
+    isLoading = false;
   });
 }
 
 var getSortedRoom = (page, key, sort) => {
+  isSort = true;
+  isLoading = true;
   $.ajax({
     type  : 'get',
     url   : 'https://murmuring-anchorage-78399.herokuapp.com/api/rooms' + "?page=" + page + "&key=" + key + "&sort=" + sort
   }).then((data) => {
     rooms = data.result;
+    if (rooms.length===0) loadDone = true;
     console.log(rooms);
     var test = template({obj: rooms});
-    document.getElementById('main_info').innerHTML = test;
+    if (currentSortPage == 1) {
+      $('#main_info').html(test);
+    } else {
+      $('#main_info').append(test);
+    }
   }).fail((err) => {
     console.error(err);
+  }).always(() => {
+    isLoading = false;
   });
 }
 
 query === '' ? getRoomsOnPage(1,'Đà Lạt Cô Tô Hội An') : getRoomsOnPage(1,newQuery);
-console.log(newQuery);
+
+$("#ascend").click(function(){
+  isAscend = true;
+  isDescend = false;
+  loadDone = false;
+  currentSortPage = 1;
+  getSortedRoom(1,'undefined',1);
+});
+
+$("#descend").click(function(){
+  isDescend = true;
+  isAscend = false;
+  loadDone = false;
+  currentSortPage = 1;
+  getSortedRoom(1,'undefined',-1);
+});
+
+$(window).on('scroll', onWindowScrolled);
+function onWindowScrolled(){
+  if(!isLoading && !loadDone && $(window).height() + $(window).scrollTop() === $(document).height()){
+    if (isSort === false) {
+      query === '' ? getRoomsOnPage(currentPage,'Đà Lạt Cô Tô Hội An') : getRoomsOnPage(currentPage,newQuery);
+      currentPage++;
+    } else {
+      if (isAscend) {
+        query === '' ? getSortedRoom(currentSortPage+1,'undefined',1) : getSortedRoom(currentSortPage+1,newQuery,1);
+        currentSortPage++;
+      } else {
+        query === '' ? getSortedRoom(currentSortPage+1,'undefined',-1) : getSortedRoom(currentSortPage+1,newQuery,-1);
+        currentSortPage++;
+      }
+    }
+  }
+}
